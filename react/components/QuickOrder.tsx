cefc97fb3cda472cf1ceef0a04fbcb63972fb073
Daniela <dannytorres0211@gmail.com>
2022-10-07 17:50:41 -0500

    style: add styles in margn and padding to show

--- react/components/QuickOrder.tsx
+++ react/components/QuickOrder.tsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useMutation, useLazyQuery } from "react-apollo";
-import UPDATE_CART from "../graphql/updateCart.graphql"
-import GET_PRODUCT from "../graphql/getProductBySku.graphql"
+import UPDATE_CART from "../graphql/updateCart.graphql";
+import GET_PRODUCT from "../graphql/getProductBySku.graphql";
+import { useCssHandles } from "vtex.css-handles";
+import "./styles.css";
 
 const QuickOrder = () => {
   const [inputText, setInputText] = useState("");
@@ -53,16 +55,28 @@ const QuickOrder = () => {
     }
   }
 
+  const CSS_HANDLES = [
+    "container__form",
+    "form__input",
+    "form__label",
+    "container__button",
+    "form__input--button"
+  ]
+
+  const handles = useCssHandles(CSS_HANDLES)
+
   return (
-    <div>
+    <div className={handles["container__form"]}>
       <form
         onSubmit={searchProduct}
       >
         <div>
-          <label htmlFor="sku">Ingresa el número de SKU</label>
-          <input id="sku" type="text" onChange={handleChange}></input>
+          <label htmlFor="sku" className={handles["form__label"]}>Ingresa el número de SKU</label>
+          <input id="sku" type="text" onChange={handleChange} className={handles["form__input"]}></input>
         </div>
-        <input type="submit" value="COMPRAR"/>
+        <div className={handles["container__button"]}>
+          <input type="submit" value="COMPRAR" className={handles["form__input--button"]}/>
+        </div>
       </form>
     </div>
   )
